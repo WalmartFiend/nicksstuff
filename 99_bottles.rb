affb9beb6028b48d7bae26da78ac76428fbab6f5
Nick N <nicknarh@gmail.com>
2014-02-23 16:15:54 -0500

    Fixed  while loop

--- 99_bottles.rb
+++ 99_bottles.rb
@@ -1,6 +1,6 @@
 beers = 99
 
-while beers > 1
+while beers > 0
   puts beers.to_s + " bottles of beer on the wall"
   puts beers.to_s + " bottles of Beer"
   puts "Take one down....pass it around"
@@ -8,4 +8,4 @@ while beers > 1
   puts beers.to_s + " " + "bottles of beer in the wall"
 end
 
-puts "0 bottles of beer on the wall"
+# puts "0 bottles of beer on the wall"
